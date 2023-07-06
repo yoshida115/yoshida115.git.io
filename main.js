3ba7510f59f4b245de59e8007ac5084175daa2c4
yoshida_eri <e.yoshida@d3-jp.com>
2023-07-06 23:50:58 +0900

    006

--- main.js
+++ main.js
@@ -8,9 +8,27 @@
 // console.log(sum);
 
 // 005
-console.log(100 + 200);
-console.log(200 - 80);
-console.log(100 * 3);
-console.log(400 / 5);
-console.log(402 % 5);
-console.log(2 ** 3);
+// console.log(100 + 200);
+// console.log(200 - 80);
+// console.log(100 * 3);
+// console.log(400 / 5);
+// console.log(402 % 5);
+// console.log(2 ** 3);
+
+// 006
+let myName = '鈴木';
+alert(myName);
+
+let number1 = 10;
+let number2 = 20;
+let sum = number1 + number2;
+console.log(sum);
+
+let familyName = '鈴木';
+let firstName = '太郎';
+let fullName = familyName + firstName;
+console.log(fullName);
+
+let value1 = 100;
+let value2 = value1;
+console.log(value2);
