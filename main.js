c7f9a813abcaec093f0fc5e84af90c6acedf6bc1
yoshida_eri <e.yoshida@d3-jp.com>
2023-07-06 23:42:38 +0900

    005

--- main.js
+++ main.js
@@ -2,7 +2,15 @@
 // alert('こんにちは');
 
 // 004
-const a = 10;
-const b = 20;
-const sum = a + b;
-console.log(sum);
+// const a = 10;
+// const b = 20;
+// const sum = a + b;
+// console.log(sum);
+
+// 005
+console.log(100 + 200);
+console.log(200 - 80);
+console.log(100 * 3);
+console.log(400 / 5);
+console.log(402 % 5);
+console.log(2 ** 3);
